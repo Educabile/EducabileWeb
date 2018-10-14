c9793516af74ebc68108aa8549eb7c37c732c0f5
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-14 17:51:34 +0200

    Void component as of now

--- src/containers/Azienda/Azienda.js
+++ src/containers/Azienda/Azienda.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Azienda = props => {
-  return (
-    <React.Fragment>
-      <h2>Ciao</h2>
-    </React.Fragment>
-  );
+  return null;
 };
 
 export default Azienda;
