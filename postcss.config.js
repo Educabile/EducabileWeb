089cf9a4b2aaa29c01cc0b201c2ad9ea04ecb41d
Claudio Cortese <claudio.cortese@outlook.it>
2018-11-15 11:00:33 +0100

    Adding more PostCSS plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   plugins: [
     require('postcss-flexbugs-fixes'),
+    require('postcss-brand-colors'),
+    require('postcss-resemble-image').default,
+    require('postcss-easing-gradients'),
+    // TODO: Load every font we use here
+    require('postcss-font-magician'),
+    require('postcss-calc'),
+    require('postcss-pseudo-content-insert'),
+    require('postcss-unique-selectors'),
+    require('postcss-ordered-values'),
+    require('postcss-normalize-url'),
+    require('postcss-normalize-charset'),
+    require('postcss-convert-values'),
+    require('postcss-merge-rules'),
+    require('postcss-will-change-transition'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009',
