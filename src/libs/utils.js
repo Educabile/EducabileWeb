a8e93f3706c42a6a8889520320cff58d03bf73b7
Claudio Cortese <claudio.cortese@outlook.it>
2018-11-17 21:30:35 +0100

    feat: add sharp to resize and convert assets to WebP

---
+++ src/libs/utils.js
@@ -0,0 +1,26 @@
+const getFileInfo = path => path.split('/').pop()
+
+const getFilePath = path =>
+  path
+    .split('/')
+    .slice(0, -1)
+    .join('/')
+
+const getFileName = path => getFileInfo(path).split('.')[0]
+
+const getFileExtension = path => getFileInfo(path).split('.')[1]
+
+const getImageSizesFromConfig = () => {
+  const { IMAGE_SIZES } = process.env
+
+  return IMAGE_SIZES.split(',')
+    .map(sizes => sizes.split('x'))
+    .map(sizes => [parseInt(sizes[0]), parseInt(sizes[1])])
+}
+
+module.exports = {
+  getFilePath,
+  getFileName,
+  getFileExtension,
+  getImageSizesFromConfig,
+}
