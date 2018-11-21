27dda4fe2e38e0569e7359a55e0575ff8f8f2c30
Claudio Cortese <claudio.cortese@outlook.it>
2018-11-21 22:05:09 +0100

    feat: add some Babel plugins
    In particular we have added the following plugins:
    - transform-react-constant-elements
    - transform-react-inline-prop-types
    - transform-imports
    
    Closes #48

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,35 @@
-const { POSTCSS_MODES } = require('@craco/craco')
+const { whenProd, POSTCSS_MODES } = require('@craco/craco')
 
-module.exports = function({ env, paths }) {
+module.exports = function() {
   return {
     style: {
       postcss: {
         mode: POSTCSS_MODES.file,
       },
     },
+    babel: whenProd(() => ({
+      plugins: [
+        '@babel/plugin-transform-react-constant-elements',
+        '@babel/plugin-transform-react-inline-elements',
+        'babel-plugin-transform-react-remove-prop-types',
+        [
+          'babel-plugin-transform-imports',
+          {
+            'react-materialize': {
+              transform: 'react-materialize/lib/${member}',
+              preventFullImport: true,
+            },
+            'react-router-dom': {
+              transform: 'react-router-dom/${member}',
+              preventFullImport: true,
+            },
+            'react-router-transition': {
+              transform: 'react-router-transition/lib/${member}',
+              preventFullImport: true,
+            },
+          },
+        ],
+      ],
+    })),
   }
 }
