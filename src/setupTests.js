e635da312cae4a824bdb302e7782b7d5c0bd7925
Claudio Cortese <claudio.cortese@outlook.it>
2018-12-03 15:22:11 +0100

    feat: add scrollspy (#66)
    Closes #65

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,10 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+
+/* eslint-disable */
+global.MutationObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe(element, initObject) {}
+}
